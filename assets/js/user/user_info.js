1a638c0ca6531347dbd5c1e217e970a6e670be2d
zlc <18229972186@163.com>
2020-10-05 10:34:53 +0800

    个人中心完成

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,53 @@
+$(function() {
+    var form = layui.form
+    var layer = layui.layer
+
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
+        }
+    })
+
+    initUserInfo()
+
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    // 重置表单的数据
+    $('#btnReset').on('click', function(e) {
+
+        e.preventDefault()
+        initUserInfo()
+    })
+
+
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
